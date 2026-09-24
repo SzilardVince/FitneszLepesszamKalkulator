fe433f1b86b166035402a516743c9ec2787fd904
SzilardVince <szilard.vince.2024d@telekimezotur.hu>
2026-09-24 13:16:00 +0200

    feladat 2

--- FitneszLepesszamKalkulator/Program.cs
+++ FitneszLepesszamKalkulator/Program.cs
@@ -1,2 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
- 
+﻿List<int> szaml = new List<int>();
+
+Console.WriteLine("=== Heti Lépésszám Rögzítése ===");
+
+for(int i=0;i<=4;i++)
+{
+    Console.Write($"{i+1}.nap lépésszáma: ");
+    szaml.Add(int.Parse(Console.ReadLine()));
+}
