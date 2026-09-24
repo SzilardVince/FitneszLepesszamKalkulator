7bbdac804fe947c07bff41a70c6f14bc9c4a3489
SzilardVince <szilard.vince.2024d@telekimezotur.hu>
2026-09-24 13:20:08 +0200

    feladat 3

--- FitneszLepesszamKalkulator/Program.cs
+++ FitneszLepesszamKalkulator/Program.cs
@@ -7,3 +7,9 @@ for(int i=0;i<=4;i++)
     Console.Write($"{i+1}.nap lépésszáma: ");
     szaml.Add(int.Parse(Console.ReadLine()));
 }
+int osz = 0;
+for (int i = 0; i <= 4; i++)
+{
+    osz += szaml[i];
+}
+double atlag = osz / 5.0;
