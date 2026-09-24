ad9547dc3017fcccf6c92ecf76c31eb78d290f8a
SzilardVince <szilard.vince.2024d@telekimezotur.hu>
2026-09-24 13:27:53 +0200

    feladat 5

--- FitneszLepesszamKalkulator/Program.cs
+++ FitneszLepesszamKalkulator/Program.cs
@@ -17,3 +17,15 @@ string telj = "";
 if (atlag >= 10000) telj = "Kiváló forma, teljesítetted a célt!";
 else if (atlag >= 7000) telj = "Átlagos aktivitás, jó úton jársz.";
 else telj = "Kevés mozgás, több aktivitás szükséges!";
+Console.WriteLine("Adatok feldolgozása...");
+Console.WriteLine("========================================");
+Console.WriteLine("Rögzitett napi lépészámok: ");
+for (int i = 0; i <= 4; i++)
+{
+    Console.WriteLine($"{i + 1}. nap: {szaml[i]} lépés");
+}
+Console.WriteLine("========================================");
+Console.WriteLine($"összes lépészám: {osz} lépés");
+Console.WriteLine($"heti atlagos lepeszam: {atlag:F2} lépés");
+Console.WriteLine($"heti értékelés: {telj}");
+Console.WriteLine("========================================");
